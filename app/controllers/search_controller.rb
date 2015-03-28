57240669c019d117834368361e91bdce4fbfeb96
jlightyear <joseluis.estrach@gmail.com>
2015-03-28 12:53:19 -0700

    Added Previous and Next product in search show

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -14,8 +14,22 @@ class SearchController < ApplicationController
   end
 
   def show
-    if params[:product_id] 
+    if params[:product_id] && !params[:searchbox].blank?
       @product = Product.find(params[:product_id])
+      @products = Product.search(params[:searchbox])
+
+      if @products[(@products.index(@product))] != @products.last
+        @next_product = @products[(@products.index(@product)) + 1]
+      else
+        @next_product = @products[0]
+      end
+
+      if @products[(@products.index(@product))] != @products[0]
+        @previous_product = @products[(@products.index(@product)) - 1]
+      else
+        @previous_product = @products.last
+      end
+
     else
       redirect_to catalogo_index_path
     end
